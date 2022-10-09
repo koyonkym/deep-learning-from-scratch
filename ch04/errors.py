712b64d0e8af1c7e2910d525347048b0047a082a
Koyo Nakayama <taiyou.003.084@gmail.com>
2022-10-09 14:14:46 +0900

    first commit

---
+++ ch04/errors.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t)**2)
+
+
+def cross_entropy_error(y, t):
+    if y.dim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(t * np.log(y + 1e-7)) / batch_size
